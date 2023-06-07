1e71b34b0816f2a5e3540368d5f8c895c222096f
root9464 <senpai9464@yandex.ru>
2023-06-07 21:11:05 +0500

    07.06.2023

--- 3.js
+++ 3.js
@@ -48,3 +48,197 @@
 // console.log(a(), b());
 
 
+// function slow(x) {
+//     // здесь могут быть ресурсоёмкие вычисления
+//     console.log(`Called with ${x}`);
+//     return x;
+//   }
+  
+//   function cachingDecorator(func) {
+//     let cache = new Map();
+  
+//     return function(x) {
+//       if (cache.has(x)) {    // если кеш содержит такой x,
+//         return cache.get(x); // читаем из него результат
+//       }
+  
+//       let result = func(x); // иначе, вызываем функцию
+  
+//       cache.set(x, result); // и кешируем (запоминаем) результат
+//       return result;
+//     };
+//   }
+  
+//   slow = cachingDecorator(slow);
+  
+//   console.log( slow(1) ); // slow(1) кешируем
+//   console.log( "Again: " + slow(1) ); // возвращаем из кеша
+  
+//   console.log( slow(2) ); // slow(2) кешируем
+//   console.log( "Again: " + slow(2) ); // возвращаем из кеша
+
+
+// const names = {
+//     one: "Egor",
+//     two: "Ivan",
+//     three: "Artem"
+// }
+// const hello = 'Hello';
+// let say = ()=>{
+//     const random = [Object.values(names)[Math.floor(Math.random()*Object.values(names).length)]]
+//     return `${hello}` + " " + random
+// }
+
+// console.log(say.call())
+
+// let worker = {
+//     someMethod() {
+//       return 1;
+//     },
+  
+//     slow(x) {
+//       console.log("Called with " + `${x}`);
+//       return x * this.someMethod(); // (*)
+//     }
+//   };
+  
+//   function cachingDecorator(func) {
+//     let cache = new Map();
+//     return function(x) {
+//       if (cache.has(x)) {
+//         return cache.get(x);
+//       }
+//       let result = func.call(this, x); // теперь 'this' передаётся правильно
+//       cache.set(x, result);
+//       return result;
+//     };
+//   }
+  
+// worker.slow = cachingDecorator(worker.slow); // теперь сделаем её кеширующей
+
+// console.log( worker.slow(2) ); // работает
+// console.log( worker.slow(2) ); // работает, не вызывая первоначальную функцию (кешируется)
+
+// let worker = {
+//     slow(min, max) {
+//       console.log(`Передано ${min},${max}`);
+//       return min + max;
+//     }
+//   };
+  
+//   function cachingDecorator(func, hash) {
+//     let cache = new Map();
+//     return function() {
+//       let key = hash(arguments); // (*)
+//       if (cache.has(key)) {
+//         return cache.get(key);
+//       }
+  
+//       let result = func.call(this, ...arguments); // (**)
+  
+//       cache.set(key, result);
+//       return result;
+//     };
+//   }
+  
+//   function hash(args) {
+//     return args[0] + ',' + args[1];
+//   }
+  
+//   worker.slow = cachingDecorator(worker.slow, hash);
+  
+//   console.log( "Результат:" + worker.slow(3, 5) ); // работает
+//   console.log( "Кешированный результат: " + worker.slow(3, 5) ); // аналогично (из кеша)
+
+// function hash() {
+//     console.log([].join.call(arguments) ); // 1,2
+//   }
+  
+// hash(4, 2);
+
+
+
+// function f(a) {
+//     console.log(a)
+//   }
+  
+//   // f1000 передаёт вызовы f максимум раз в 1000 мс
+// let f1000 = throttle(f, 1000);
+
+// f1000(1); // показывает 1
+// f1000(2); // (ограничение, 1000 мс ещё нет)
+// f1000(3); // (ограничение, 1000 мс ещё нет)
+// function throttle(func, ms) {
+
+//     let isThrottled = false,
+//       savedArgs,
+//       savedThis;
+  
+//     function wrapper() {
+  
+//       if (isThrottled) { // (2)
+//         savedArgs = arguments;
+//         savedThis = this;
+//         return;
+//       }
+  
+//       func.apply(this, arguments); // (1)
+  
+//       isThrottled = true;
+  
+//       setTimeout(function() {
+//         isThrottled = false; // (3)
+//         if (savedArgs) {
+//           wrapper.apply(savedThis, savedArgs);
+//           savedArgs = savedThis = null;
+//         }
+//       }, ms);
+//     }
+  
+//     return wrapper;
+//   }
+// console.log(throttle(f, 1000)(5)); // показывает 1);
+
+
+
+// let user = {
+//     firstName: "Вася",
+//     sayHi() {
+//       console.log("Привет" + " " + `${user.firstName}`);
+//     }
+//   };
+  
+//   setTimeout(user.sayHi, 1000); 
+
+// let user = {
+//     firstName: "Вася",
+//     sayHi() {
+//       console.log(`Привет, ${this.firstName}!`);
+//     }
+//   };
+  
+//   setTimeout((event) => {
+//     user.sayHi(); 
+//   }, 1000);
+
+// let user = {
+//     firstName: "Вася"
+// };
+// function func() {
+//     return this.firstName
+//   }
+  
+//   let funcUser = func.bind(user);
+//   funcUser(); // Вася
+
+let user = {
+    firstName: "Вася",
+    say(phrase) {
+      console.log(`${phrase}, ${this.firstName}!`);
+    }
+  };
+  
+  let say = user.say.bind(user);
+  
+  say("Привет"); // Привет, Вася (аргумент "Привет" передан в функцию "say")
+  say("Пока");
