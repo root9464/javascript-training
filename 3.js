4155a35a40c73c2e7c32f2f5a8f9f16af35f7d4e
root9464 <senpai9464@yandex.ru>
2023-06-11 19:50:49 +0500

    11.06.2023

--- 3.js
+++ 3.js
@@ -231,14 +231,35 @@
 //   let funcUser = func.bind(user);
 //   funcUser(); // Вася
 
-let user = {
-    firstName: "Вася",
-    say(phrase) {
-      console.log(`${phrase}, ${this.firstName}!`);
-    }
-  };
+// let user = {
+//     firstName: "Вася",
+//     say(phrase) {
+//       console.log(`${phrase}, ${this.firstName}!`);
+//     }
+//   };
   
-  let say = user.say.bind(user);
+//   let say = user.say.bind(user);
   
-  say("Привет"); // Привет, Вася (аргумент "Привет" передан в функцию "say")
-  say("Пока");
+//   say("Привет"); // Привет, Вася (аргумент "Привет" передан в функцию "say")
+//   say("Пока");
+
+// function partial(func, ...argsBound) {
+//   return function(...args) { // (*)
+//     return func.call(this, ...argsBound, ...args);
+//   }
+// }
+
+// // использование:
+// let user = {
+//   firstName: "John",
+//   say(time, phrase) {
+//     console.log(`[${time}] ${this.firstName}: ${phrase}!`);
+//   }
+// };
+
+// // добавляем частично применённый метод с фиксированным временем
+// user.sayNow = partial(user.say, new Date().getHours() + ':' + new Date().getMinutes());
+
+// user.sayNow("Hello");
+// // Что-то вроде этого:
+// // [10:00] John: Hello!
---
+++ 4.js
@@ -0,0 +1,108 @@
+////свойства обьекта и их конфигурация
+
+// let user = {
+//     name: "John",
+//     surname: "FFF"
+//   };
+  
+//   let descriptor = Object.getOwnPropertyDescriptor(user, 'surname');
+  
+//   console.log( JSON.stringify(descriptor, null, 2 ) );
+
+// let user = {};
+
+// Object.defineProperty(user, "name", {
+//   value: "John"
+// });
+
+// let descriptor = Object.getOwnPropertyDescriptor(user, 'name');
+
+// console.log( JSON.stringify(descriptor, null, 2 ) );
+
+// "use strict"
+//     let user = {
+//         name: "John"
+//     };
+    
+//     Object.defineProperty(user, "name", {
+//     writable: false
+//     });
+
+//     user.name = "Pete";
+//     console.log(user.name); 
+
+
+// let user = {
+//     name: "John",
+//     surname: "Smith",
+  
+//     get fullName() {
+//       return `${this.name} ${this.surname}`;
+//     }
+//   };
+  
+//   console.log(user.fullName); // John Smith
+
+
+// let user = {
+//     name: "John",
+//     surname: "Smith",
+  
+//     get fullName() {
+//       return `${this.name} ${this.surname}`;
+//     },
+  
+//     set fullName(value) {
+//       [this.name, this.surname] = value.split(" ");
+//     }
+//   };
+  
+//   // set fullName запустится с данным значением
+//   user.fullName = "Alice Cooper";
+  
+//   console.log(user.name); // Alice
+//   console.log(user.surname); // Cooper
+
+
+// let user = {
+//     name: "John",
+//     surname: "Smith"
+//   };
+  
+//   Object.defineProperty(user, 'fullName', {
+//     get() {
+//       return `${this.name} ${this.surname}`;
+//     },
+  
+//     set(value) {
+//       [this.name, this.surname] = value.split(" ");
+//     }
+//   });
+// let fullnameKey = {
+
+// }
+// for(let key in user) {
+//     fullnameKey[key] = user[key];
+
+// } // name, surname
+// console.log(fullnameKey)
+
+
+// let user = {
+//     get name(){
+//         return this._name
+//     },
+//     set name(value){
+//         if (value.length  <= 4){
+//             console.log("слишком короткое")
+//         }
+//         this._name = value
+//     }
+
+// }
+// user.name = "Pet"
+// console.log(user.name)
+
+
+
+
